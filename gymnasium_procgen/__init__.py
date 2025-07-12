058d7808199e69d3a0297e59d362c0ed567a720f
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2025-07-12 19:00:41 +0100

    Add cibuildtool

--- gymnasium_procgen/__init__.py
+++ gymnasium_procgen/__init__.py
@@ -1,10 +1,11 @@
 """Procgen2 - Procedurally Generated Game-Like RL Environments."""
 
-__version__ = "1.0.0"
+import importlib.metadata
 
 from gymnasium_procgen.env import ProcgenEnv, ProcgenGym3Env
 from gymnasium_procgen.registration import register_environments
 
-register_environments()
-
+__version__ = importlib.metadata.version("mypackage")
 __all__ = ["ProcgenEnv", "ProcgenGym3Env", "__version__"]
+
+register_environments()
